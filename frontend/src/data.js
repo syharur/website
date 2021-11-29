cc864e48df694de42543cf8e7450e66f853d3c53
syharur <syahrur11@yahoo.com>
2021-11-29 11:04:20 +0800

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            nama:'Fortuner',
+            category:'Car',
+            image:'/image/fortuner.jpg',
+            price:30001,
+            brand:'toyota',
+            rating:4.5,
+            numReviews:10,
+            description:'sport car'
+        },
+        {
+            _id:'2',
+            nama:'Fortuner',
+            category:'Car',
+            image:'image/fortuner.jpg',
+            price:30002,
+            brand:'toyota',
+            rating:4.5,
+            numReviews:10,
+            description:'sport car'
+        },
+        {
+            _id:'3',
+            nama:'Harrier',
+            category:'Car',
+            image:'/image/New-Harrier-Specs.jpg',
+            price:30003,
+            brand:'toyota',
+            rating:4.5,
+            numReviews:10,
+            description:'sport car'
+        },
+        {
+            _id:'4',
+            nama:'Fortuner',
+            category:'Car',
+            image:'/image/fortuner.jpg',
+            price:30004,
+            brand:'toyota',
+            rating:4.5,
+            numReviews:10,
+            description:'sport car'
+        },
+        {
+            _id:'5',
+            nama:'Fortuner',
+            category:'Car',
+            image:'/image/fortuner.jpg',
+            price:30005,
+            brand:'toyota',
+            rating:4.5,
+            numReviews:10,
+            description:'sport car'
+        },
+        {
+            _id:'6',
+            nama:'Fortuner',
+            category:'Car',
+            image:'/image/fortuner.jpg',
+            price:30006,
+            brand:'toyota',
+            rating:4.5,
+            numReviews:10,
+            description:'sport car'
+        },
+    
+    ]
+};
+export default data;
